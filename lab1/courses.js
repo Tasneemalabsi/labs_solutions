7ad276977852473c674aec9849230a581ff6c6a3
Tasnim Shaker Al Absi <t.alabsi@ltuc.com>
2022-12-29 14:38:19 +0300

    lab4 finished

---
+++ lab1/courses.js
@@ -0,0 +1,7 @@
+let stars = Number(prompt('rate the website from 1 to 5'))
+
+let image = "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSHP9QFoDtbpUaMhI7c8NNWp143Ck1aoya_aEO4niK&s' width='50' height='50'>"
+
+for(let i = 1; i<=stars; i++){
+    document.write(image)
+}
